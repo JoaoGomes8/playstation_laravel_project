6f1c06e45dabb49f74a34c916e44a233724eb023
Joao <joaogomes2609@gmail.com>
2026-03-17 15:15:32 +0000

    Primeiro envio do projeto

---
+++ resources/views/utils/home.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.main')
+
+@section('content')
+    <div class="row mb-4">
+        <div class="col-12">
+            <h1 class="display-5 fw-bold">Estúdios PlayStation</h1>
+            <p class="lead">Explora os jogos dos teus estúdios favoritos.</p>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-12">
+            <div class="alert alert-info" role="alert">
+                A listagem de estúdios vinda da base de dados vai aparecer aqui!
+            </div>
+        </div>
+    </div>
+@endsection
